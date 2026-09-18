ef33729558ba7726139676532333de718dcc5c68
ErlanyDeKun <erlanydekun@gmail.com>
2026-09-18 03:40:49 +0300

    Add files via upload

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        CheckedFunc();
+        UncheckedFunc();
+
+        void CheckedFunc()
+        {
+            Console.WriteLine("=== checked ===");
+            try
+            {
+                checked
+                {
+                    int max = int.MaxValue;
+                    Console.WriteLine($"max      = {max}");
+                    int result = max + 1;
+                    Console.WriteLine($"max + 1  = {result}");
+                }
+            }
+            catch (OverflowException e)
+            {
+                Console.WriteLine($"Исключение: {e.Message}");
+            }
+        }
+
+        void UncheckedFunc()
+        {
+            Console.WriteLine("\n=== unchecked ===");
+            unchecked
+            {
+                int max = int.MaxValue; 
+                Console.WriteLine($"max      = {max}");
+                int result = max + 1;
+                Console.WriteLine($"max + 1  = {result}");
+            }
+        }
+    }
+}
